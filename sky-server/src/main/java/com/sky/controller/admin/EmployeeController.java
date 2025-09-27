f6569ee96b466081bd91278e5cf52fb5710db50e
blackHand <3203530480@qq.com>
2025-09-28 00:33:08 +0800

    启用禁用员工账号

--- sky-server/src/main/java/com/sky/controller/admin/EmployeeController.java
+++ sky-server/src/main/java/com/sky/controller/admin/EmployeeController.java
@@ -88,7 +88,6 @@ public class EmployeeController {
 
     /**
      * 员工分页查询
-     *
      * @param employeePageQueryDTO
      * @return
      */
@@ -98,6 +97,20 @@ public class EmployeeController {
         log.info("员工分页查询，参数为：{}", employeePageQueryDTO);
         PageResult pageResult = employeeService.pageQuery(employeePageQueryDTO);
         return Result.success(pageResult);
+    }
+
+    /**
+     * 启用禁用员工账号
+     * @param status
+     * @param id
+     * @return
+     */
+    @PostMapping("/status/{status}")
+    @ApiOperation("启用禁用员工账号")
+    public Result startOrStop(@PathVariable Integer status,Long id){
+        log.info("启用禁用员工账号：{},{}", status, id);
+        employeeService.startOrStop(status,id);
+        return Result.success();
     }
 
 }
